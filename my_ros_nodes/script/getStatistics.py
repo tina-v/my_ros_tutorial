203ea02bc5e7c5e31ec0e80bb8fe749b44428c10
Tina <4van@informatik.uni-hamburg.de>
2018-11-15 21:22:09 +0100

    added customized msg type into getCount.msg, script getStatistic.py working now

--- my_ros_nodes/script/getStatistics.py
+++ my_ros_nodes/script/getStatistics.py
@@ -5,38 +5,44 @@ from std_msgs.msg import String
 # from my_ros_msgs import SpeakingNmber
 from my_ros_msgs.msg import SpeakingNumber
 # importing service GetCount
-from my_ros_nodes.srv import GetCountResponse
+from my_ros_nodes.srv import GetCountResponse, GetCount
 # rospy 
 import rospy
 
 
 #processing msg
-#def handleStatistics():
-#   count = my_ros_nodes.svr.GetCount(lastmsg)
-#   rospy.loginfo(firstmsg + lastmsg + count)
+def handleStatistics(msg):
 #test
-#    rospy.loginfo("")
+    rospy.loginfo(GetCountResponse(countmsgs, firstmsg, lastmsg))
 
-#updatesCount
+#updates received msgcount, saves information of SpeakingNumber as global variables
 def updateCount(msg):
-    global firstmsg 
+    global firstmsg
     global lastmsg
+    global countmsgs
+
     firstmsg = SpeakingNumber()
     lastmsg = SpeakingNumber()
+    countmsgs = 0
 
     if firstmsg is None: 
         firstmsg = msg
+        countmsgs = 1
 
+    countmsgs += 1
     lastmsg = msg
 
-#test    rospy.loginfo(firstmsg)
-#test    rospy.loginfo(lastmsg)
+#test    handleStatistics(msg)
+
+#test
+#    rospy.loginfo(firstmsg)
+#    rospy.loginfo(lastmsg)
 
 #
 def getStatistic():
     rospy.init_node('getStatistic_node', anonymous=True)
     sub = rospy.Subscriber("speaking_numbers", SpeakingNumber, updateCount)
-    service = rospy.Service("getCount", GetCountResponse, updateCount)
+    service = rospy.Service("getCount", GetCount, handleStatistics)
 # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
